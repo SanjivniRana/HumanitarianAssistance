47e5730645ba8828fbd7dd6a4659882f5534e701
Bipul Singh <bipuls@smartdatainc.net>
2017-11-21 18:55:13 +0530

    Changes done for Office Code , Currency Code and Email Setting module.

--- src/app/dashboard/code/code.service.ts
+++ src/app/dashboard/code/code.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import {Http, Headers, Response, RequestOptions, RequestOptionsArgs} from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { CurrencyCode, OfficeCode, OfficeCodefordelete } from '../../Models/CodeModel';
 
 //chartOfAccounts
 export class ChartOfAccount {
@@ -160,8 +163,119 @@ let journalCodes: JournalCode[] = [
     },
 ];
 
+
+export class CurrencyData{
+    CurrencyId: string;
+    CurrencyCode: string;
+    CurrencyName: string;
+    CurrencyRate: DoubleRange;
+}
+
+let currencydata: CurrencyData = {
+    "CurrencyId": "",
+    "CurrencyCode": "",
+    "CurrencyName": "",
+    "CurrencyRate": null
+};
+
+export class OfficeData{
+    OfficeId : any;
+    OfficeCode : string;
+    OfficeName : string;
+    SupervisorName : string;
+    PhoneNo : string;
+    FaxNo : string;
+}
+
+let officedata: OfficeData ={
+    "OfficeId" : "",
+    "OfficeCode" : "",
+    "OfficeName" : "",
+    "SupervisorName" : "",
+    "PhoneNo" : "",
+    "FaxNo" : ""
+}
+
+export class EmailSettingData{
+    EmailId : any;
+    SenderEmail : string;
+    EmailType : any;
+    SenderPassword : string;
+    SmtpPort : any;
+    SmtpServer : string;
+    EnableSSL : boolean;
+}
+
+let emailsetting: EmailSettingData = {
+    "EmailId" : "",
+    "SenderEmail" : "",
+    "EmailType" : "",
+    "SenderPassword" : "",
+    "SmtpPort" : "",
+    "SmtpServer" : "",
+    "EnableSSL" : false
+}
+
+let emailtypes: string[] = [
+    'General','Bidding Panel'];
+
+export class EmailSettingTempData{
+    EmailId : any;
+    SenderEmail : string;
+    EmailType : any;
+    SenderPassword : string;
+    SmtpPort : any;
+    SmtpServer : string;
+    EnableSSL : boolean;
+}
+
+let emailsettingtempdata: EmailSettingTempData[] = [{
+    "EmailId": 1,
+    "SenderEmail": "admin",
+    "EmailType": "General",
+    "SenderPassword": "11800",
+    "SmtpPort": "4000",
+    "SmtpServer": "192.155.246.146",
+    "EnableSSL": true
+}, {
+    "EmailId": 2,
+    "SenderEmail": "admin",
+    "EmailType": "General",
+    "SenderPassword": "11800",
+    "SmtpPort": "4000",
+    "SmtpServer": "192.155.246.146",
+    "EnableSSL": true
+}, {
+    "EmailId": 3,
+    "SenderEmail": "admin",
+    "EmailType": "General",
+    "SenderPassword": "11800",
+    "SmtpPort": "4000",
+    "SmtpServer": "192.155.246.146",
+    "EnableSSL": true
+}, {
+    "EmailId": 4,
+    "SenderEmail": "supperadmin",
+    "EmailType": "Bidding Panel",
+    "SenderPassword": "11800",
+    "SmtpPort": "4000",
+    "SmtpServer": "192.155.246.146",
+    "EnableSSL": true
+}, {
+    "EmailId": 5,
+    "SenderEmail": "supperadmin",
+    "EmailType": "Bidding Panel",
+    "SenderPassword": "11800",
+    "SmtpPort": "4000",
+    "SmtpServer": "192.155.246.146",
+    "EnableSSL": false
+}];
+
 @Injectable()
 export class CodeService {
+
+    constructor(private http: Http) {}
+
     //Manage Chat of account
 
     //chartOfAccounts
@@ -182,4 +296,149 @@ export class CodeService {
     getJournalCodes() {
         return journalCodes;
     }
+
+
+
+    getCurrencyData() : CurrencyData
+    {
+        currencydata.CurrencyId = "";
+        currencydata.CurrencyCode = "";
+        currencydata.CurrencyName = "";
+        currencydata.CurrencyRate = null;
+        return currencydata;
+    }
+
+    GetAllCodeList(url: string) 
+    {
+        debugger;
+        let Myheaders = new Headers();
+        Myheaders.append("Authorization", "Bearer " + localStorage.getItem("authenticationtoken"));
+        let options = new RequestOptions({ headers: Myheaders });
+        return this.http.get(url, options)
+            .map((response: Response) => {
+                let codelist = response.json();
+                if (codelist) {
+                    return codelist;
+                }
+            }).catch(this.handleError); 
+    }
+
+    AddEditCurrencyCode(url: string, model: CurrencyCode) {
+        debugger;
+        let Myheaders = new Headers();
+        Myheaders.append("Authorization", "Bearer " + localStorage.getItem("authenticationtoken"));
+        Myheaders.append("Content-Type", "application/json");
+        let options = new RequestOptions({ headers: Myheaders });
+        
+        let a=new RequestOptions();
+        let b=
+        {
+            CurrencyId : model.CurrencyId,
+            CurrencyCode : model.CurrencyCode,
+            CurrencyName : model.CurrencyName,
+            CurrencyRate : model.CurrencyRate        
+        }
+                
+        return this.http.post(url, JSON.stringify(b)
+        ,options)            
+        .map((response: Response) => {
+                let journal = response.json();
+                if (journal) {
+                    return journal;
+                }
+            }).catch(this.handleError);
+
+    }
+
+    getOfficeData() : OfficeData
+    {
+        officedata.OfficeId = "";
+        officedata.OfficeCode = "";
+        officedata.OfficeName = "";
+        officedata.SupervisorName = "";
+        officedata.PhoneNo = "";
+        officedata.FaxNo = "";
+        return officedata;
+    }
+
+    AddEditOfficeCode(url: string, model: OfficeCode) {
+        debugger;
+        let Myheaders = new Headers();
+        Myheaders.append("Authorization", "Bearer " + localStorage.getItem("authenticationtoken"));
+        Myheaders.append("Content-Type", "application/json");
+        let options = new RequestOptions({ headers: Myheaders });
+        
+        let a=new RequestOptions();
+        let b=
+        {
+            OfficeId : model.OfficeId,
+            OfficeCode : model.OfficeCode,
+            OfficeName : model.OfficeName,
+            SupervisorName : model.SupervisorName,
+            PhoneNo : model.PhoneNo,
+            FaxNo : model.FaxNo
+        }
+                
+        return this.http.post(url, JSON.stringify(b)
+        ,options)            
+        .map((response: Response) => {
+                let journal = response.json();
+                if (journal) {
+                    return journal;
+                }
+            }).catch(this.handleError);
+
+    }
+
+    DeleteOfficeCode(url: string, model: OfficeCodefordelete) {
+        debugger;
+        let Myheaders = new Headers();
+        Myheaders.append("Authorization", "Bearer " + localStorage.getItem("authenticationtoken"));
+        Myheaders.append("Content-Type", "application/json");
+        let options = new RequestOptions({ headers: Myheaders });
+        
+        let a=new RequestOptions();
+        let b=
+        {
+            OfficeId : model.OfficeId
+        }
+                
+        return this.http.post(url, JSON.stringify(b)
+        ,options)            
+        .map((response: Response) => {
+                let journal = response.json();
+                if (journal) {
+                    return journal;
+                }
+            }).catch(this.handleError);
+
+    }
+
+
+    getEmailSettingData() : EmailSettingData
+    {
+        emailsetting.EmailId = "";
+        emailsetting.SenderEmail = "";
+        emailsetting.EmailType = "";
+        emailsetting.SenderPassword = "";
+        emailsetting.SmtpPort = "";
+        emailsetting.SmtpServer = "";
+        emailsetting.EnableSSL = false;
+        return emailsetting;
+    }
+
+    getEmailTypes() {
+        return emailtypes;
+    }
+
+    getEmailSetting(): EmailSettingTempData[] {
+        return emailsettingtempdata;
+    }
+
+    private handleError(error: Response) 
+        {        
+            console.log(error.json());
+            return Observable.throw(error.json().error || 'Server error');
+    
+        }
 }
