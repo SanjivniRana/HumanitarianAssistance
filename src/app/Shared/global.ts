cd3c9e9a82c448d0507d496526500abecaeab3a4
shubhamk <shubhamk@SDD-65>
2017-12-02 16:05:53 +0530

    chart of accounts changes

--- src/app/Shared/global.ts
+++ src/app/Shared/global.ts
@@ -1,22 +1,22 @@
-export class GLOBAL { 
+export class GLOBAL {
     public static API_Login_Auth_Url = 'Account/Login';
-    public static API_Account_GetUserType =  'Account/UserTypes';
+    public static API_Account_GetUserType = 'Account/UserTypes';
     public static API_AllOffice_URL = 'Code/GetAllOfficeDetails';
     public static API_AllDepartment_Url = 'Account/GetDepartment';
-    public static API_Common_GetProviderByMaster="";
-    public static API_UserDetail_AddUser ='Account/AddUsers';
-    public static API_UserDetail_GetUserList ='Account/GetAllUserDetails';
+    public static API_Common_GetProviderByMaster = "";
+    public static API_UserDetail_AddUser = 'Account/AddUsers';
+    public static API_UserDetail_GetUserList = 'Account/GetAllUserDetails';
     public static API_UserRoles_GetRolesList = 'Account/GetRoles';
     public static API_UserRoles_GetUserRolesByUserId = 'Account/GetUserRole';
     public static API_UserRoles_AssignRoleToUser = 'Account/AssignRoleToUser';
     public static API_Permissions_GetPermissionsByRoleId = 'Account/GetPermissionByRoleId';
     public static API_Permissions_GetPermissions = 'Account/GetPermissions';
     public static API_Permissions_AddPermissionInRoles = 'Account/PermissionsInRoles';
-    public static API_CheckCurrentPassword ="Account/CheckCurrentPassword";
+    public static API_CheckCurrentPassword = "Account/CheckCurrentPassword";
     public static API_ChangePassword = "Account/ChangePassword";
     public static API_ResetPassword = "Account/ResetPassword";
     public static API_UserDetail_GetUserDetailsByUserId = 'Account/GetUserDetailsByUserId';
-    public static API_UserDetail_EditUser ='Account/EditUser'; 
+    public static API_UserDetail_EditUser = 'Account/EditUser';
     public static API_CurrencyCodes_GetAllCurrency = 'Code/GetAllCurrency';
     public static API_CurrencyCodes_AddCurrency = 'Code/AddCurrency';
     public static API_CurrencyCode_EditCurrency = 'Code/EditCurrency';
@@ -43,4 +43,10 @@ export class GLOBAL {
     public static API_Accounting_EditVouchersTransaction = 'Account/EditVoucherTransactionDetail';
     public static API_Accounting_GetAllJournalDetails = 'Account/GetJouranlVoucherDetails';
     public static API_Accounting_GetAllLedgerDetails = 'Account/GetAllLedgerDetails';
+
+    //Alpit 30-11-2017
+    public static API_ChartOfAccount_GetAllChartAccountDetail = 'Code/GetAllChartAccountDetail';
+    public static API_Accounting_GetAllAccoutnType = 'Code/GetAllAccountType';
+    public static API_ChartOfAccount_AddChartAccountDetail = 'Code/AddChartAccountDetail';
+    public static API_ChartOfAccount_EditChartAccountDetail = 'Code/EditChartAccountDetail';
 }
